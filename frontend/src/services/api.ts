ec13cdce335140b6845c673f21988eaeb93885d6
Angela Dube <AngelaDube+capitec@capitecbank.co.za>
2026-09-08 23:23:07 +0200

    added changes

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import type { Branch } from "../types/branch";
 
 export async function fetchBranches(): Promise<Branch[]> {
-    const response = await axios.get<Branch[]>('http://localhost:3001/api/branches')
+    const BFF_URL = import.meta.env.DEV ? 'http://localhost:3001' : ''
+    const response = await axios.get<Branch[]>(`${BFF_URL}/api/branches`)
     return response.data
 }
 
