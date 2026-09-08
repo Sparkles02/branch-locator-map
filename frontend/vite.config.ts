0fcda6361279180af6f46af246220838a083eb49
Angela Dube <AngelaDube+capitec@capitecbank.co.za>
2026-09-08 23:12:35 +0200

    fix vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,6 @@
-/// <reference types="vitest" />
-  import react from '@vitejs/plugin-react'
-  import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-  export default defineConfig({
-    plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: './src/setupTests.ts',
-    },
-  })
+export default defineConfig({
+  plugins: [react()],
+})
